b8a924ee14e078b166467f36adcd35f63e62d0cc
agun <agunggumelar02@gmail.com>
2019-07-16 11:45:46 +0700

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+class App extends React.Component {
+    onSearchSubmit(terms){
+        console.log(terms);
+        
+    }
+
+    render() {
+        return (
+            <div className="ui container" style={{marginTop: '10px'}}>
+                <SearchBar onSubmit={this.onSearchSubmit}/>
+            </div>
+        );
+    }
+};
+
+export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class SearchBar extends React.Component{
+    state = { terms: 'Hi loo'};
+   
+    onInputChange(event){
+        console.log(event.target.value);
+    }
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        this.props.onSubmit(this.state.terms);
+    }
+    
+    render() {
+        return (
+            <div className="ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <label>Image Search</label>
+                        <input 
+                            type='text' 
+                            value={this.state.terms}
+                            onChange={e => this.setState({terms: e.target.value})} />
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
